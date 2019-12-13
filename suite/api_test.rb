b0e3e044e8aa851ed0f2b890a560590ccbfd8e2d
minokus <m.d.novik@gmail.com>
2019-12-13 03:32:57 -0700

    Итерация 6
    Раскоментил твой тест, когда ищешь по thomas эксепшенс не вылетают
    
    Эрроры ты сам увидишь, один кажется ожидаемый(или его нужно ловить?). С линком не разобрался, кажтся делаю по примеру с rest client, но что-то идет не так.

--- suite/api_test.rb
+++ suite/api_test.rb
@@ -8,12 +8,14 @@ SEARCH_THOMAS='thomas'
 class ApiTest < Minitest::Test
   include RequestHelper
 
+  # 2. Add an assertion to suite/test_no_api_key to ensure the response at runtime matches what is currently displayed with the api key missing
   def test_no_api_key
     request('GET', '?s=star', {}, 'http://www.omdbapi.com/')
     assert_equal last_response.obj['Response'], "False"
     assert_equal last_response.obj['Error'], "No API key provided."
   end
 
+  # 3. Extend suite/api_test.rb by creating a test that performs a search on 'thomas'.
   def test_search_thomas
     request('GET', "?s=#{SEARCH_THOMAS}&apikey=#{API_KEY}", {}, 'http://www.omdbapi.com/')
     last_response.obj['Search'].map { |item|
@@ -26,18 +28,15 @@ class ApiTest < Minitest::Test
     }
   end
 
+  # 4. Add a test that uses the i parameter to verify each title on page 1 is accessible via imdbID
   def test_i_parameter
         request('GET', "?i=#{}&page=#{'1'}&apikey=#{API_KEY}", {}, 'http://www.omdbapi.com/')
-       # item = last_response.obj['Search'][0]
-       # request('GET', "?i=#{item['imdbID']}&apikey=#{API_KEY}", {}, 'http://www.omdbapi.com/')
         assert_equal last_response.obj['Title'].nil?, false
-
-
   end
 
+  # 5. Add a test that verifies none of the poster links on page 1 are broken
   def test_links_arent_broken
     request('GET', "?i=#{}&page=#{'1'}&apikey=#{API_KEY}", {}, 'http://www.omdbapi.com/')
-
     assert_equal last_response.RestClient.get(Poster) , 200
 
   end
