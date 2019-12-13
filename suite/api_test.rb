4fde7ffa29e9ab2a6fece05ec4fa4ea31bf8ccf6
minokus <m.d.novik@gmail.com>
2019-12-13 02:28:00 -0700

    Скобки мля

--- suite/api_test.rb
+++ suite/api_test.rb
@@ -3,7 +3,7 @@ require File.expand_path('../support/test_helper', __dir__)
 require 'minitest/autorun'
 
 API_KEY='9ed3a540'
-SEARCH_TOMAS='thomas'
+SEARCH_THOMAS='thomas'
 
 class ApiTest < Minitest::Test
   include RequestHelper
@@ -15,30 +15,29 @@ class ApiTest < Minitest::Test
   end
 
   def test_search_thomas
-    request('GET', "?s=#{SEARCH_TOMAS}&apikey=#{API_KEY}", {}, 'http://www.omdbapi.com/')
+    request('GET', "?s=#{SEARCH_THOMAS}&apikey=#{API_KEY}", {}, 'http://www.omdbapi.com/')
     last_response.obj['Search'].map { |item, key|
-      # assert_equal item['Title'].downcase.include?(SEARCH_TOMAS), true # Not all title has latin a. Skipped this test, because of fail
+      # assert_equal item['Title'].downcase.include?(SEARCH_THOMAS), true # Not all title has latin a. Skipped this test, because of fail
 
       ['Title', 'Year', 'imdbID', 'Type', 'Poster'].map { |prop|
         assert_equal item[prop].nil?, false
         assert_equal item[prop].is_a?(String), true
       }
-
+    }
+  end
       # Verify year matches correct format ??? - What is correct format?
   def test_i_parameter
       if key === 1
         request('GET', "?i=#{item['imdbID']}&apikey=#{API_KEY}", {}, 'http://www.omdbapi.com/')
         assert_equal last_response.obj['Title'].nil?, false
+      end
   end
 
   def test_links_arent_broken
-    request('GET', "?s=#{SEARCH_TOMAS}&apikey=#{API_KEY}", {}, 'http://www.omdbapi.com/')
+    request('GET', "?s=#{SEARCH_THOMAS}&apikey=#{API_KEY}", {}, 'http://www.omdbapi.com/')
     assert_equal last_response.code, 200
     #    request(last_response.obj['Poster'], 200)
 
         #assert_equal last_response.code, 200
   end
-
-
-
-
+end
